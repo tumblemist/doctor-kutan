aca07ae10526c5c1383f4fba0eacbbc10d179f88
tumblemist <procrastinated@gmail.com>
2016-12-13 02:20:15 -0600

    Update doctor.js

--- doctor.js
+++ doctor.js
@@ -2,13 +2,27 @@ var Discord = require("discord.js");
 var config = require("./config");
 var bot = new Discord.Client();
 
-bot.on("message", msg => {
-    if (msg.content.startsWith("ping")) {
-      msg.channel.sendMessage("pong!");
-    } 
+var responseObject = {
+  "ayy": "Ayy, lmao!",
+  "wat": "Say what?",
+  "lol": "roflmaotntpmp"
+};
 
-    else if (msg.content.startsWith("depart")) {
-      bot.destroy();
+bot.on("ready", () => {
+    console.log(`Ready to server in ${bot.channels.size} channels on ${bot.guilds.size} servers, for a total of ${bot.users.size} users.`);
+});
+
+bot.on("message", msg => {   
+    let prefix = "!";
+    if(!msg.content.startsWith(prefix)) return;
+    if(msg.author.bot) return;
+    
+    if(msg.content.startswith(prefix + responseObject[message.content])) {
+        message.channel.sendMessage(responseObject[message.content]);
+     }
+
+    else if (msg.content.startsWith(prefix + "depart")) {
+        bot.destroy();
     }
 });
 

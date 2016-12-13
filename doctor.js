dcfd187a41640182fdd814652ac035c346fc9bd4
tumblemist <procrastinated@gmail.com>
2016-12-13 02:01:47 -0600

    Create doctor.js

---
+++ doctor.js
@@ -0,0 +1,19 @@
+var Discord = require("discord.js");
+var config = require("config");
+var bot = new Discord.Client();
+
+bot.on("message", msg => {
+    if (msg.content.startsWith("ping")) {
+      msg.channel.sendMessage("pong!");
+    } 
+
+    else if (msg.content.startsWith("depart")) {
+      bot.destroy();
+    }
+});
+
+bot.on('ready', () => {
+  console.log('I am ready!');
+});
+
+bot.login(config.token);
